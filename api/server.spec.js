8b73aef91f5685946f3c003cc8d2723e7f8eb23e
James Grantham <james.r.grantham@gmail.com>
2019-11-20 20:41:05 +0000

    Server test passing

---
+++ api/server.spec.js
@@ -0,0 +1,43 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server', () => {
+  describe('base endpoint', () => {
+    test('the db env is testing', () => {
+      expect(process.env.DB_ENV).toBe('testing');
+    });
+
+    test('should return 200 OK', async () => {
+      const response = await request(server).get('/');
+      expect(response.status).toBe(200);
+    });
+
+    test('should return 200 OK with ES6 promise', () => {
+      return request(server)
+        .get('/')
+        .then(response => {
+          expect(response.status).toBe(200);
+        });
+    });
+
+    test('with supertest syntax', () => {
+      return request(server)
+        .get('/')
+        .expect(200)
+        .expect( '"server running"' )
+        .expect('Content-Length', '16')
+        .expect('Content-Type', /json/);
+    });
+
+    test('returns the right response body', async () => {
+      const response = await request(server).get('/');
+      expect(response.body).toEqual('server running');
+    });
+  });
+
+  // describe('auth router', () => {
+  //   test('the db env is testing', () => {
+  //     expect(process.env.DB_ENV).toBe('testing');
+  //   });
+  // })
+});
---
+++ routers/auth-router.spec.js
@@ -0,0 +1,68 @@
+const request = require('supertest')
+const server = require('../api/server')
+const db = require('../database/dbConfig')
+
+const registerApi = '/api/auth/register'
+const loginApi = '/api/auth/login'
+
+const exampleUser = {
+  username: 'exampleUser',
+  password: 'examplePassword'
+}
+
+beforeEach(async () => {
+  await db('users').truncate()
+  await request(server)
+  .post(registerApi)
+  .send(exampleUser)
+})
+
+describe('auth-router', () => {
+  describe('register endpoint', () => {
+    it('should have a body', async () => {
+      const response = await request(server)
+        .post(registerApi)
+        .send({
+          username: 'Tony',
+          password: '1234'
+        })
+      // console.log(response.body)
+      expect(response.body.username).toEqual('Tony')
+    })
+    // this is what went late **********************************
+    it('should not allow duplicates', async () => {
+      const response = await request(server)
+        .post(registerApi)
+        .send(exampleUser)
+        console.log(response.res.text);
+        expect(response.res.text).toContain('SQLITE_CONSTRAINT')
+    })
+  })
+
+
+  describe('login endpoint', () => {
+    it('should return a token', async () => {
+      const loginResponse = await request(server)
+        .post(loginApi)
+        .send(exampleUser)
+      // console.log(loginResponse.status)
+      // let token = loginResponse.body.token
+      expect(loginResponse.body).toHaveProperty('token')
+      expect(loginResponse.status).toBe(200)
+    })
+  })
+  describe('login endpoint', () => {
+    it("shouldn't return a token", async () => {
+      const loginResponse = await request(server)
+        .post(loginApi)
+        .send({
+          username: 'does not exist',
+          password: 'incorrect'
+        })
+      // console.log(loginResponse.status)
+      // let token = loginResponse.body.token
+      expect(loginResponse.status).toBe(401)
+    })
+  })
+})
+
